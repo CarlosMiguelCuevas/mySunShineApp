a5c83e0196bd231970d772d0e657d6d051d04657
CarlosMiguelDesarrollo <carlos.cuevas.desarrollo@gmail.com>
2016-07-01 11:34:02 -0700

    Revert "some changes that I dont want anymore", to redo the database thing
    This reverts commit 42a180d9ba9d97cba162d3be57d78ff428951857.

--- app/src/main/java/mx/com/cubozsoft/sunshineapp/listaPrincipal/MainActivity.java
+++ app/src/main/java/mx/com/cubozsoft/sunshineapp/listaPrincipal/MainActivity.java
@@ -3,7 +3,6 @@ package mx.com.cubozsoft.sunshineapp.listaPrincipal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -20,43 +19,12 @@ public class MainActivity extends AppCompatActivity implements ListOfWeather.OnF
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v("state","OnCreate");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.generalmenu,menu);
         return true;
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.v("state","OnDestroy");
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.v("state","OnStop");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.v("state","OnPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.v("state","OnResume");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.v("state","OnStart");
     }
 
     @Override
