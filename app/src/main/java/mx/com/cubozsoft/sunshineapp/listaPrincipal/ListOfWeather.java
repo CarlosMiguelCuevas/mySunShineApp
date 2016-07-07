24a49e20ac60957974fccf999879739b9e17a883
CarlosMiguelDesarrollo <carlos.cuevas.desarrollo@gmail.com>
2016-07-07 14:58:13 -0700

    delet the new instance contructor in the ListFragment, and delet the params that I dont use

--- app/src/main/java/mx/com/cubozsoft/sunshineapp/listaPrincipal/ListOfWeather.java
+++ app/src/main/java/mx/com/cubozsoft/sunshineapp/listaPrincipal/ListOfWeather.java
@@ -44,65 +44,23 @@ import mx.com.cubozsoft.sunshineapp.WifiConectorReciever;
 import mx.com.cubozsoft.sunshineapp.data.WeatherContract;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ListOfWeather.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ListOfWeather#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ListOfWeather extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    static final String ARG_PARAM1 = "param1";
-    static final String ARG_PARAM2 = "param2";
+
     Cursor mDataList;
     BroadcastReceiver mReceiver;
-
     RecyclerView mRecyclerView;
     RecyclerView.Adapter mAdapter;
     RecyclerView.LayoutManager mManager;
-
-    // TODO: Rename and change types of parameters
-    String mParam1;
-    String mParam2;
-
     OnFragmentInteractionListener mListener;
 
     public ListOfWeather() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListOfWeather.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListOfWeather newInstance(String param1, String param2) {
-        ListOfWeather fragment = new ListOfWeather();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
         setHasOptionsMenu(true);
-
-
     }
 
     @Override
